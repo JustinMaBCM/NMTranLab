36b4ad9375ca3d49d7c1584d7f30d8dac8ccba38
JustinMaBCM <162752954+JustinMaBCM@users.noreply.github.com>
2024-07-22 12:41:04 -0500

    Update Ma_Functions.R

--- Ma_CMO-Labeling_of_RGCs/scripts/Ma_Functions.R
+++ Ma_CMO-Labeling_of_RGCs/scripts/Ma_Functions.R
@@ -1,1 +1,11 @@
+CR0.8_Assign <- function(DF, CMOnum = 6){
+  DFtemp <- DF[,c(2:(CMOnum+1),(CMOnum+3):(CMOnum+6), CMOnum+2)]
+  DFtemp$CR0.8 <- 0
+  for (i in 1:nrow(DFtemp)){
+    if (DFtemp$Assignment_Probability[i] >= 0.8) {DFtemp$CR0.8[i] <- colnames(DFtemp)[which(match(DFtemp[i,1:(CMOnum+2)],DFtemp[i,(CMOnum+4)]) == 1)]
+    } else {DFtemp$CR0.8[i] <- "Unassigned"}
+  }
+  return(DFtemp$CR0.8)
+}
+
 
